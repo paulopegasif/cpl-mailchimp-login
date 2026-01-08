8974cd769e91aa4b9b75673c5698b9f739d92c6c
Paulo Henrique Silva <paulohenrique.psilva@a.ucb.br>
2026-01-08 10:29:09 -0300

    chore: Atualiza versão para 2.0.4
    Correções aplicadas:
    - Detecção flexível de formulários do Elementor
    - Melhora na validação e tratamento de erros
    - Corrige fechamento de popup após autenticação
    - Resolve problema de página em branco
    - Logs detalhados de debug no console

--- cpl-mailchimp-login.php
+++ cpl-mailchimp-login.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CPL Mailchimp Login
  * Description: Valida acesso às aulas conferindo e-mail e tag no Mailchimp, com configurações avançadas.
- * Version: 2.0.0
+ * Version: 2.0.4
  * Author: Paulo Silva - ELPH
  */
 
